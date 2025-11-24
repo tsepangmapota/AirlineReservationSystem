58d20bfa4f8c2e6a5bc13c7be568dbe7f173c9b7
Neomokhachane <neomokhachane787@gmail.com>
2025-11-24 13:46:40 +0200

    Update Member6Controller.java

--- src/main/java/com/airline/Controller/Member6Controller.java
+++ src/main/java/com/airline/Controller/Member6Controller.java
@@ -14,6 +14,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
 import java.net.URL;
 import java.sql.*;
 import java.time.LocalDate;
